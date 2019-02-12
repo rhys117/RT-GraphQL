44e2d0445e59dbbed1a8a6d083bf281cbf46b328
rhys117 <rhys117@gmail.com>
2019-02-12 22:37:13 +1000

    changes to how to find comments

--- app/models/rt/ticket.rb
+++ app/models/rt/ticket.rb
@@ -12,10 +12,11 @@ class RT::Ticket < ActiveRecord::Base
   has_one :requestor_group, -> { where type: 'Requestor' }, class_name: 'Group', foreign_key: :instance
   has_many :requestors, through: :requestor_group, class_name: 'User', source: :users
 
-  has_many :transactions, class_name: 'Transaction', foreign_key: :objectid
-  has_many :attachments, through: :transactions, class_name: 'Attachment', source: :attachment
-  has_many :comment_transactions, -> { where "type = ? OR type = ?", 'Comment', 'Create' }, class_name: 'Transaction', foreign_key: :objectid
-  has_many :comments, through: :comment_transactions, class_name: 'Attachment', source: :attachment
+  # Below does not attribute to merged tickets transactions
+  # has_many :transactions, class_name: 'Transaction', foreign_key: :objectid
+  # has_many :attachments, through: :transactions, class_name: 'Attachment', source: :attachment
+  # has_many :comment_transactions, -> { where "type = ? OR type = ?", 'Comment', 'Create' }, class_name: 'Transaction', foreign_key: :objectid
+  # has_many :comments, through: :comment_transactions, class_name: 'Attachment', source: :attachment
 
   has_one :owner_obj, class_name: 'User', primary_key: :creator, foreign_key: :id
   has_one :creator, class_name: 'User', primary_key: :creator, foreign_key: :id
@@ -28,6 +29,23 @@ class RT::Ticket < ActiveRecord::Base
   def self.find(id)
     id = RT::Ticket.select(:effectiveid).unscoped.where(id: id).limit(1)&.first&.effectiveid
     super(id)
+  end
+
+  def merged_ticket_ids
+    RT::Ticket.select(:id).unscoped.where(effectiveid: id)
+  end
+
+  def transactions
+    RT::Transaction.where(objectid: merged_ticket_ids)
+  end
+
+  def transaction_ids
+    transactions.select(:id)
+  end
+
+  def comments
+    relevant_trans = transactions.where("type = ? OR type = ?", 'Comment', 'Create')
+    relevant_trans.map { |ent| RT::Attachment.find_by_transactionid(ent.id) }
   end
 
   # This setup is to avoid stack to deep errors due to RT database structure
